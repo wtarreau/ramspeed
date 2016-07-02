ae825a0e298f1fd47f1bedb4b7be12a7540ce445
Willy Tarreau <w@1wt.eu>
2016-07-02 11:05:08 +0200

    ramlat: implement memory latency measurements
    This program measures the memory latency, or more exactly the transaction
    rate on random addresses per word size and per area size.
    
    The principle consists in walking over the memory using random addresses
    and to measure how many accesses are possible per unit of time. The base
    addresses are all multiples of 64 bytes so that it is possible to measure
    the time taken to retrieve 1 to 64 bytes without being badly affected by
    random cache line crossing.
    
    An initial version was imperfect because it was using a fast xorshift
    random pattern generator which used to consume CPU cycles and registers,
    hence used to produce much lower numbers than expected, especially in
    L1/L2 caches.
    
    This new version drops the xorshift in favor of a modular arithmetic
    approach, consisting in adding a constant prime value to the existing
    address. In order to limit the number of operations, a fixed prime is
    chosen (257), multiplied by 512, and accesses within the 512-byte range
    are hard-coded using simple offsets that most CPUs can trivially add to
    a pointer. This results in a random enough pattern that prevents any
    form of prefetching from being performed by the cache controller and
    which emphasizes the differences between in-cache and out-of-cache
    performance.
    
    The values are read by the compiler as the argument to an empty "asm"
    statement that doesn't use them. It ensures they are not optimized away
    and that the compiler does its best to fetch the data. Whenever possible
    we try to read multiple words in the same asm statement. That avoids the
    write-after-write dependency which happens when the compiler wants to
    read everything using the same register because it doesn't know that we
    didn't use the value previously fetched.
    
    On ix86 (32-bit), there are too few registers and the performance suffers
    when multiple reads are passed to the same asm statement because the last
    read touches a register that is also used somewhere else in the loop. For
    this reason we check the architecture to either perform two consecutive
    reads or a double one.
    
    It is worth mentionning that on all archs tested (i586, x86_64, mips,
    armv7t, armv8), performing a 64-bit read at once is much more efficient
    than implementing it as two 32-bit reads, apparently because the compiler
    knows that it can always use a certain pair of registers and produces
    better code this way. So now instead of deciding whether we read 64 bits
    at once based on the word size, we only decide based on the risk of
    register shortage (ie ix86) whether we read a single 64-bit integer or
    two at once.

---
+++ ramlat.c
@@ -0,0 +1,540 @@
+#include <sys/time.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#if defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__)
+#define HAS_MANY_REGISTERS 0
+#else
+#define HAS_MANY_REGISTERS 1
+#endif
+
+#define LOOPS_PER_ROUND 65536
+
+/* set once the end is reached, reset when setting an alarm */
+volatile static int stop_now;
+
+static inline uint64_t rdtsc()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+/* reads 8 bits from memory area <area>, offset <off> as efficiently as
+ * possible for the current architecture.
+ */
+static inline void read8(char *area, off_t off)
+{
+	asm volatile("" : : "r" (*(uint8_t *)(area + off)));
+}
+
+/* same with two addresses at once */
+static inline void read8_dual(char *area, off_t off1, off_t off2)
+{
+	if (HAS_MANY_REGISTERS) {
+		asm volatile("" : : "r" (*(uint8_t *)(area + off1)), "r" (*(uint8_t *)(area + off2)));
+	}
+	else {
+		asm volatile("" : : "r" (*(uint8_t *)(area + off1)));
+		asm volatile("" : : "r" (*(uint8_t *)(area + off2)));
+	}
+}
+
+/* reads 16 bits from memory area <area>, offset <off> as efficiently as
+ * possible for the current architecture.
+ */
+static inline void read16(char *area, off_t off)
+{
+	asm volatile("" : : "r" (*(uint16_t *)(area + off)));
+}
+
+/* same with two addresses at once */
+static inline void read16_dual(char *area, off_t off1, off_t off2)
+{
+	if (HAS_MANY_REGISTERS) {
+		asm volatile("" : : "r" (*(uint16_t *)(area + off1)), "r" (*(uint16_t *)(area + off2)));
+	}
+	else {
+		asm volatile("" : : "r" (*(uint16_t *)(area + off1)));
+		asm volatile("" : : "r" (*(uint16_t *)(area + off2)));
+	}
+}
+
+/* reads 32 bits from memory area <area>, offset <off> as efficiently as
+ * possible for the current architecture.
+ */
+static inline void read32(char *area, off_t off)
+{
+	asm volatile("" : : "r" (*(uint32_t *)(area + off)));
+}
+
+/* same with two addresses at once */
+static inline void read32_dual(char *area, off_t off1, off_t off2)
+{
+	if (HAS_MANY_REGISTERS) {
+		asm volatile("" : : "r" (*(uint32_t *)(area + off1)), "r" (*(uint32_t *)(area + off2)));
+	}
+	else {
+		asm volatile("" : : "r" (*(uint32_t *)(area + off1)));
+		asm volatile("" : : "r" (*(uint32_t *)(area + off2)));
+	}
+}
+
+/* reads 64 bits from memory area <area>, offset <off> as efficiently as
+ * possible for the current architecture.
+ */
+static inline void read64(char *area, off_t off)
+{
+	asm volatile("" : : "r" (*(uint64_t *)(area + off)));
+}
+
+/* same with two addresses at once */
+static inline void read64_dual(char *area, off_t off1, off_t off2)
+{
+	if (HAS_MANY_REGISTERS) {
+		asm volatile("" : : "r" (*(uint64_t *)(area + off1)), "r" (*(uint64_t *)(area + off2)));
+	}
+	else {
+		asm volatile("" : : "r" (*(uint64_t *)(area + off1)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off2)));
+	}
+}
+
+/* reads 128 bits from memory area <area>, offset <off> as efficiently as
+ * possible for the current architecture.
+ */
+static inline void read128(char *area, off_t off)
+{
+	if (HAS_MANY_REGISTERS) {
+		asm volatile("" : : "r" (*(uint64_t *)(area + off)), "r" (*(uint64_t *)(area + off + 8)));
+	}
+	else {
+		asm volatile("" : : "r" (*(uint64_t *)(area + off)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 8)));
+	}
+}
+
+/* reads 256 bits from memory area <area>, offset <off> as efficiently as
+ * possible for the current architecture.
+ */
+static inline void read256(char *area, off_t off)
+{
+	if (HAS_MANY_REGISTERS) {
+		asm volatile("" : : "r" (*(uint64_t *)(area + off)),      "r" (*(uint64_t *)(area + off + 8)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 16)), "r" (*(uint64_t *)(area + off + 24)));
+	}
+	else {
+		asm volatile("" : : "r" (*(uint64_t *)(area + off)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 8)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 16)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 24)));
+	}
+}
+
+/* reads 512 bits from memory area <area>, offset <off> as efficiently as
+ * possible for the current architecture.
+ */
+static inline void read512(char *area, off_t off)
+{
+	if (HAS_MANY_REGISTERS) {
+		asm volatile("" : : "r" (*(uint64_t *)(area + off)),      "r" (*(uint64_t *)(area + off + 8)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 16)), "r" (*(uint64_t *)(area + off + 24)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 32)), "r" (*(uint64_t *)(area + off + 40)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 48)), "r" (*(uint64_t *)(area + off + 56)));
+	}
+	else {
+		asm volatile("" : : "r" (*(uint64_t *)(area + off)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 8)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 16)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 24)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 32)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 40)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 48)));
+		asm volatile("" : : "r" (*(uint64_t *)(area + off + 56)));
+	}
+}
+
+/* just marks the alarm as received */
+void alarm_handler(int sig)
+{
+	stop_now = 1;
+}
+
+/* sets an alarm to trigger after <usec> microseconds. 0 disables it */
+void set_alarm(unsigned int usec)
+{
+	struct itimerval timer = {
+		.it_value.tv_usec = usec % 1000000,
+		.it_value.tv_sec  = usec / 1000000,
+	};
+
+	if (usec) {
+		stop_now = 0;
+		signal(SIGALRM, alarm_handler);
+		signal(SIGVTALRM, alarm_handler);
+	}
+	setitimer(ITIMER_VIRTUAL, &timer, NULL);
+}
+
+/* returns a mask to cover the nearest lower power of two for <size> */
+static size_t mask_rounded_down(size_t size)
+{
+	size_t mask = size;
+	int shift = 1;
+
+	while (shift < 8 * sizeof(mask) && (size = mask >> shift)) {
+		mask |= size;
+		shift <<= 1;
+	}
+	return mask >> 1;
+}
+
+/* Randomly accesses aligned words of size <word> bytes over <size> bytes of
+ * area <area> for about <usec> microseconds, then returns the number of words
+ * read per microsecond. Note: size is rounded down the the lower power of two.
+ * It is mandatory that <word> is a power of two.
+ */
+unsigned int random_read_over_area(void *area, unsigned int usec, size_t size, size_t word)
+{
+	size_t mask;
+	unsigned int rounds, loop;
+	uint64_t before, after;
+	off_t rnd = 0;
+
+	mask = mask_rounded_down(size);
+	mask &= ~(word - 1);
+	mask &= ~(256UL | 128UL | 64UL); // these ones are manipulated in the loops
+	memset(area, 0, mask + 1);
+	rounds = 0;
+
+	set_alarm(usec);
+	before = rdtsc();
+
+	switch (word) {
+	case 1:
+		for (rounds = 0; !stop_now; rounds++) {
+			for (loop = 0; loop < LOOPS_PER_ROUND; loop += 16) {
+				register off_t addr1, addr2;
+
+				/* Walk following a pseudo-random pattern and limit redundancy.
+				 * A 512-byte address space is crossed following pseudo-random
+				 * moves within 64 byte locations and for each we test both the
+				 * position and its mirror over the address mask. This ensures
+				 * non-contiguous accesses that prevent any streaming operation
+				 * from being performed.
+				 */
+				rnd += 0x20200; // that's 257(prime) times 512
+
+				addr1 = rnd   & mask;
+				addr2 = addr1 ^ mask;
+				read8_dual(area, addr1 + 0,   addr2 + 0);
+				read8_dual(area, addr1 + 256, addr2 + 256);
+				read8_dual(area, addr1 + 128, addr2 + 128);
+				read8_dual(area, addr1 + 384, addr2 + 384);
+				read8_dual(area, addr1 + 320, addr2 + 320);
+				read8_dual(area, addr1 + 64,  addr2 + 64);
+				read8_dual(area, addr1 + 192, addr2 + 192);
+				read8_dual(area, addr1 + 448, addr2 + 448);
+			}
+		}
+		break;
+	case 2:
+		for (rounds = 0; !stop_now; rounds++) {
+			for (loop = 0; loop < LOOPS_PER_ROUND; loop += 16) {
+				register off_t addr1, addr2;
+
+				/* Walk following a pseudo-random pattern and limit redundancy.
+				 * A 512-byte address space is crossed following pseudo-random
+				 * moves within 64 byte locations and for each we test both the
+				 * position and its mirror over the address mask. This ensures
+				 * non-contiguous accesses that prevent any streaming operation
+				 * from being performed.
+				 */
+				rnd += 0x20200; // that's 257(prime) times 512
+
+				addr1 = rnd   & mask;
+				addr2 = addr1 ^ mask;
+				read16_dual(area, addr1 + 0,   addr2 + 0);
+				read16_dual(area, addr1 + 256, addr2 + 256);
+				read16_dual(area, addr1 + 128, addr2 + 128);
+				read16_dual(area, addr1 + 384, addr2 + 384);
+				read16_dual(area, addr1 + 320, addr2 + 320);
+				read16_dual(area, addr1 + 64,  addr2 + 64);
+				read16_dual(area, addr1 + 192, addr2 + 192);
+				read16_dual(area, addr1 + 448, addr2 + 448);
+			}
+		}
+		break;
+	case 4:
+		for (rounds = 0; !stop_now; rounds++) {
+			for (loop = 0; loop < LOOPS_PER_ROUND; loop += 16) {
+				register off_t addr1, addr2;
+
+				/* Walk following a pseudo-random pattern and limit redundancy.
+				 * A 512-byte address space is crossed following pseudo-random
+				 * moves within 64 byte locations and for each we test both the
+				 * position and its mirror over the address mask. This ensures
+				 * non-contiguous accesses that prevent any streaming operation
+				 * from being performed.
+				 */
+				rnd += 0x20200; // that's 257(prime) times 512
+
+				addr1 = rnd   & mask;
+				addr2 = addr1 ^ mask;
+				read32_dual(area, addr1 + 0,   addr2 + 0);
+				read32_dual(area, addr1 + 256, addr2 + 256);
+				read32_dual(area, addr1 + 128, addr2 + 128);
+				read32_dual(area, addr1 + 384, addr2 + 384);
+				read32_dual(area, addr1 + 320, addr2 + 320);
+				read32_dual(area, addr1 + 64,  addr2 + 64);
+				read32_dual(area, addr1 + 192, addr2 + 192);
+				read32_dual(area, addr1 + 448, addr2 + 448);
+			}
+		}
+		break;
+	case 8: {
+		for (rounds = 0; !stop_now; rounds++) {
+			for (loop = 0; loop < LOOPS_PER_ROUND; loop += 16) {
+				register off_t addr1, addr2;
+
+				/* Walk following a pseudo-random pattern and limit redundancy.
+				 * A 512-byte address space is crossed following pseudo-random
+				 * moves within 64 byte locations and for each we test both the
+				 * position and its mirror over the address mask. This ensures
+				 * non-contiguous accesses that prevent any streaming operation
+				 * from being performed.
+				 */
+				rnd += 0x20200; // that's 257(prime) times 512
+
+				addr1 = rnd   & mask;
+				addr2 = addr1 ^ mask;
+				read64_dual(area, addr1 + 0,   addr2 + 0);
+				read64_dual(area, addr1 + 256, addr2 + 256);
+				read64_dual(area, addr1 + 128, addr2 + 128);
+				read64_dual(area, addr1 + 384, addr2 + 384);
+				read64_dual(area, addr1 + 320, addr2 + 320);
+				read64_dual(area, addr1 + 64,  addr2 + 64);
+				read64_dual(area, addr1 + 192, addr2 + 192);
+				read64_dual(area, addr1 + 448, addr2 + 448);
+			}
+		}
+		break;
+	}
+	case 16:
+		for (rounds = 0; !stop_now; rounds++) {
+			for (loop = 0; loop < LOOPS_PER_ROUND; loop += 16) {
+				register off_t addr1, addr2;
+
+				/* Walk following a pseudo-random pattern and limit redundancy.
+				 * A 512-byte address space is crossed following pseudo-random
+				 * moves within 64 byte locations and for each we test both the
+				 * position and its mirror over the address mask. This ensures
+				 * non-contiguous accesses that prevent any streaming operation
+				 * from being performed.
+				 */
+				rnd += 0x20200; // that's 257(prime) times 512
+
+				addr1 = rnd   & mask; // @0 mod 512
+				read128(area, addr1);
+				addr2 = addr1 ^ mask; // @0 mod 512
+				read128(area, addr2);
+
+				addr1 += 256;  // @256 mod 512
+				read128(area, addr1);
+				addr2 += 256;  // @256 mod 512
+				read128(area, addr2);
+
+				addr1 -= 128;  // @128 mod 512
+				read128(area, addr1);
+				addr2 -= 128;  // @128 mod 512
+				read128(area, addr2);
+
+				addr1 += 256;  // @384 mod 512
+				read128(area, addr1);
+				addr2 += 256;  // @384 mod 512
+				read128(area, addr2);
+
+				addr1 -= 64;   // @320 mod 512
+				read128(area, addr1);
+				addr2 -= 64;   // @320 mod 512
+				read128(area, addr2);
+
+				addr1 -= 256;  // @64 mod 512
+				read128(area, addr1);
+				addr2 -= 256;  // @64 mod 512
+				read128(area, addr2);
+
+				addr1 += 128;  // @192 mod 512
+				read128(area, addr1);
+				addr2 += 128;  // @192 mod 512
+				read128(area, addr2);
+
+				addr1 += 256;  // @448 mod 512
+				read128(area, addr1);
+				addr2 += 256;  // @448 mod 512
+				read128(area, addr2);
+			}
+		}
+		break;
+	case 32:
+		for (rounds = 0; !stop_now; rounds++) {
+			for (loop = 0; loop < LOOPS_PER_ROUND; loop += 16) {
+				register off_t addr1, addr2;
+
+				/* Walk following a pseudo-random pattern and limit redundancy.
+				 * A 512-byte address space is crossed following pseudo-random
+				 * moves within 64 byte locations and for each we test both the
+				 * position and its mirror over the address mask. This ensures
+				 * non-contiguous accesses that prevent any streaming operation
+				 * from being performed.
+				 */
+				rnd += 0x20200; // that's 257(prime) times 512
+
+				addr1 = rnd   & mask; // @0 mod 512
+				read256(area, addr1);
+				addr2 = addr1 ^ mask; // @0 mod 512
+				read256(area, addr2);
+
+				addr1 += 256;  // @256 mod 512
+				read256(area, addr1);
+				addr2 += 256;  // @256 mod 512
+				read256(area, addr2);
+
+				addr1 -= 128;  // @128 mod 512
+				read256(area, addr1);
+				addr2 -= 128;  // @128 mod 512
+				read256(area, addr2);
+
+				addr1 += 256;  // @384 mod 512
+				read256(area, addr1);
+				addr2 += 256;  // @384 mod 512
+				read256(area, addr2);
+
+				addr1 -= 64;   // @320 mod 512
+				read256(area, addr1);
+				addr2 -= 64;   // @320 mod 512
+				read256(area, addr2);
+
+				addr1 -= 256;  // @64 mod 512
+				read256(area, addr1);
+				addr2 -= 256;  // @64 mod 512
+				read256(area, addr2);
+
+				addr1 += 128;  // @192 mod 512
+				read256(area, addr1);
+				addr2 += 128;  // @192 mod 512
+				read256(area, addr2);
+
+				addr1 += 256;  // @448 mod 512
+				read256(area, addr1);
+				addr2 += 256;  // @448 mod 512
+				read256(area, addr2);
+			}
+		}
+		break;
+	case 64:
+		for (rounds = 0; !stop_now; rounds++) {
+			for (loop = 0; loop < LOOPS_PER_ROUND; loop += 16) {
+				register off_t addr1, addr2;
+
+				/* Walk following a pseudo-random pattern and limit redundancy.
+				 * A 512-byte address space is crossed following pseudo-random
+				 * moves within 64 byte locations and for each we test both the
+				 * position and its mirror over the address mask. This ensures
+				 * non-contiguous accesses that prevent any streaming operation
+				 * from being performed.
+				 */
+				rnd += 0x20200; // that's 257(prime) times 512
+
+				addr1 = rnd   & mask; // @0 mod 512
+				read512(area, addr1);
+				addr2 = addr1 ^ mask; // @0 mod 512
+				read512(area, addr2);
+
+				addr1 += 256;  // @256 mod 512
+				read512(area, addr1);
+				addr2 += 256;  // @256 mod 512
+				read512(area, addr2);
+
+				addr1 -= 128;  // @128 mod 512
+				read512(area, addr1);
+				addr2 -= 128;  // @128 mod 512
+				read512(area, addr2);
+
+				addr1 += 256;  // @384 mod 512
+				read512(area, addr1);
+				addr2 += 256;  // @384 mod 512
+				read512(area, addr2);
+
+				addr1 -= 64;   // @320 mod 512
+				read512(area, addr1);
+				addr2 -= 64;   // @320 mod 512
+				read512(area, addr2);
+
+				addr1 -= 256;  // @64 mod 512
+				read512(area, addr1);
+				addr2 -= 256;  // @64 mod 512
+				read512(area, addr2);
+
+				addr1 += 128;  // @192 mod 512
+				read512(area, addr1);
+				addr2 += 128;  // @192 mod 512
+				read512(area, addr2);
+
+				addr1 += 256;  // @448 mod 512
+				read512(area, addr1);
+				addr2 += 256;  // @448 mod 512
+				read512(area, addr2);
+			}
+		}
+		break;
+	}
+
+	after = rdtsc();
+	set_alarm(0);
+
+	/* speed = transactions per microsecond. Use 64-bit computations to avoid
+	 * overflows. The caller can turn this into bytes per second by multiplying
+	 * by <word>.
+	 */
+	usec = after - before;
+	if (usec < 1)
+		usec = 1;
+	rounds *= LOOPS_PER_ROUND;
+	return rounds / usec;
+}
+
+
+int main(int argc, char **argv)
+{
+	unsigned int usec;
+	size_t size, size_max;
+	void *area;
+	unsigned int ret, word;
+
+	usec = 100000;
+	size_max = 1048576;
+
+	if (argc > 1)
+		usec = atoi(argv[1]) * 1000;
+
+	if (argc > 2)
+		size_max = atol(argv[2]) * 1024;
+
+	area = malloc(size_max);
+
+	printf("   size:     4B     8B    16B    32B    64B\n");
+	for (size = 1024; size <= size_max; size *= 2) {
+		printf("%6uk: ", (unsigned int)(size >> 10U));
+		for (word = 4; word <= 64; word *= 2) {
+			ret = random_read_over_area(area, usec, size, word);
+			printf("%6u ", ret);
+		}
+		printf("\n");
+	}
+	exit(0);
+}
