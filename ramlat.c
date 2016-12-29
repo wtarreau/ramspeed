7e4b47c507965c79d36a2e8574a6e849c67b4e2b
Willy Tarreau <w@1wt.eu>
2016-12-29 20:10:04 +0100

    ramlat: adapt read64_dual to follow the same pattern as read64_quad_armv7
    We also simplified the prototype, and removed the unused read32().
    The 64-bit performance didn't change.

--- ramlat.c
+++ ramlat.c
@@ -269,19 +269,14 @@ unsigned int run32_generic(void *area, size_t mask)
  *                              64-bit accesses                              *
  *****************************************************************************/
 
-static inline void read64(const char *addr, const unsigned long ofs)
-{
-	asm volatile("" : : "r" (*(uint64_t *)(addr + ofs)));
-}
-
-static inline void read64_dual(const char *addr, const unsigned long ofs1, const unsigned long ofs2)
+static inline void read64_dual(const char *addr, const unsigned long ofs)
 {
 	if (HAS_MANY_REGISTERS) {
-		asm volatile("" : : "r" (*(uint64_t *)(addr + ofs1)), "r" (*(uint64_t *)(addr + ofs2)));
+		asm volatile("" : : "r" (*(uint64_t *)(addr + ofs)), "r" (*(uint64_t *)(addr + ofs + 512)));
 	}
 	else {
-		asm volatile("" : : "r" (*(uint64_t *)(addr + ofs1)));
-		asm volatile("" : : "r" (*(uint64_t *)(addr + ofs2)));
+		asm volatile("" : : "r" (*(uint64_t *)(addr + ofs)));
+		asm volatile("" : : "r" (*(uint64_t *)(addr + ofs + 512)));
 	}
 }
 
@@ -303,41 +298,47 @@ unsigned int run64_generic(void *area, size_t mask)
 			 */
 			addr = area + (rnd & mask);
 
-			read64_dual(addr + 0000,   0, 512 +   0);
-			read64_dual(addr + 0000, 256, 512 + 256);
-			read64_dual(addr + 0000, 128, 512 + 128);
-			read64_dual(addr + 0000, 384, 512 + 384);
-			read64_dual(addr + 0000, 320, 512 + 320);
-			read64_dual(addr + 0000,  64, 512 +  64);
-			read64_dual(addr + 0000, 192, 512 + 192);
-			read64_dual(addr + 0000, 448, 512 + 448);
+			read64_dual(addr,   0);
+			read64_dual(addr, 128);
+			read64_dual(addr, 256);
+			read64_dual(addr, 384);
+			read64_dual(addr,  64);
+			read64_dual(addr, 192);
+			read64_dual(addr, 320);
+			read64_dual(addr, 448);
 
-			read64_dual(addr + 1024,   0, 512 +   0);
-			read64_dual(addr + 1024, 256, 512 + 256);
-			read64_dual(addr + 1024, 128, 512 + 128);
-			read64_dual(addr + 1024, 384, 512 + 384);
-			read64_dual(addr + 1024, 320, 512 + 320);
-			read64_dual(addr + 1024,  64, 512 +  64);
-			read64_dual(addr + 1024, 192, 512 + 192);
-			read64_dual(addr + 1024, 448, 512 + 448);
+			addr += 1024;
 
-			read64_dual(addr + 2048,   0, 512 +   0);
-			read64_dual(addr + 2048, 256, 512 + 256);
-			read64_dual(addr + 2048, 128, 512 + 128);
-			read64_dual(addr + 2048, 384, 512 + 384);
-			read64_dual(addr + 2048, 320, 512 + 320);
-			read64_dual(addr + 2048,  64, 512 +  64);
-			read64_dual(addr + 2048, 192, 512 + 192);
-			read64_dual(addr + 2048, 448, 512 + 448);
+			read64_dual(addr,   0);
+			read64_dual(addr, 128);
+			read64_dual(addr, 256);
+			read64_dual(addr, 384);
+			read64_dual(addr,  64);
+			read64_dual(addr, 192);
+			read64_dual(addr, 320);
+			read64_dual(addr, 448);
 
-			read64_dual(addr + 3072,   0, 512 +   0);
-			read64_dual(addr + 3072, 256, 512 + 256);
-			read64_dual(addr + 3072, 128, 512 + 128);
-			read64_dual(addr + 3072, 384, 512 + 384);
-			read64_dual(addr + 3072, 320, 512 + 320);
-			read64_dual(addr + 3072,  64, 512 +  64);
-			read64_dual(addr + 3072, 192, 512 + 192);
-			read64_dual(addr + 3072, 448, 512 + 448);
+			addr += 1024;
+
+			read64_dual(addr,   0);
+			read64_dual(addr, 128);
+			read64_dual(addr, 256);
+			read64_dual(addr, 384);
+			read64_dual(addr,  64);
+			read64_dual(addr, 192);
+			read64_dual(addr, 320);
+			read64_dual(addr, 448);
+
+			addr += 1024;
+
+			read64_dual(addr,   0);
+			read64_dual(addr, 128);
+			read64_dual(addr, 256);
+			read64_dual(addr, 384);
+			read64_dual(addr,  64);
+			read64_dual(addr, 192);
+			read64_dual(addr, 320);
+			read64_dual(addr, 448);
 		}
 	}
 	return rounds;
